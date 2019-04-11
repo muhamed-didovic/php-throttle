ac3e13a10a55d743c176c6c6830d389312c53087
Muhamed Didovic <muhamed.didovic@gmail.com>
2019-04-11 12:43:46 +0200

    resolve issue with class name

--- src/Transformers/RequestTransformer.php
+++ src/Transformers/RequestTransformer.php
@@ -1,7 +1,5 @@
 <?php
 
-//declare(strict_types=1);
-
 namespace MuhamedDidovic\Throttle\Transformers;
 
 use MuhamedDidovic\Throttle\Data;
@@ -10,19 +8,19 @@ use MuhamedDidovic\Throttle\Data;
  * This is the request transformer class.
  *
  */
-class ZendRequestTransformer implements TransformerInterface
+class RequestTransformer implements TransformerInterface
 {
     /**
      * Transform the data into a new data instance.
      *
-     * @param \Illuminate\Http\Request $data
-     * @param                       $limit
-     * @param                       $time
+     * @param $data
+     * @param $limit
+     * @param $time
      *
-     * @return \MuhamedDidovic\Throttle\Data
+     * @return Data
      */
     public function transform($data, $limit = 10, $time = 60)
     {
-        return new Data((string) $data->getClientIp(), (string) $data->getPathInfo(), (int) $limit, (int) $time);
+        return new Data((string)$data->getClientIp(), (string)$data->getPathInfo(), (int)$limit, (int)$time);
     }
 }
